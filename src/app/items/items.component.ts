64801f1be737786fe1828a5f5aaf4dda721f578e
Eran Meir <eranmeir1z@gmail.com>
2021-04-04 17:27:25 +0300

    1

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.css']
+  styleUrls: ['./items.component.css'],
 })
 export class ItemsComponent implements OnInit {
+  images = [
+    { name: 'Column 1', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 2', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 3', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 4', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 4', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 4', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 4', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 4', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 4', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 4', value: 'https://via.placeholder.com/150' },
+    { name: 'Column 4', value: 'https://via.placeholder.com/150' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
